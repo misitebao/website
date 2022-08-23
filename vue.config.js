0a0ad4ad8104981ae45f5d02a5c1b6ec0b7776c4
Misite Bao <i@misitebao.com>
2022-08-23 19:13:56 +0800

    feat: update meta info

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
       // 在 dist/index.html 的输出
       filename: 'index.html',
       // 当使用 title 选项时，
-      title: 'Homepage',
+      title: 'Misite Bao - Technology serves evolution rather than control',
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
